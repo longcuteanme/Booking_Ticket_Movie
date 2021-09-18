115ff2878a96db0603e19e4bf00103e139dbd74c
long Trinh <psatrinhlongat48@gmail.com>
2021-09-18 15:35:35 +0700

    change github link href

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -76,7 +76,7 @@ function MyComponent() {
             />
           </td>
           <td className="p-2">
-            <a className="text-sm text-white" href="https://github.com/longcuteanme/bookingTicketMovie">
+            <a className="text-sm text-white" href="https://github.com/longcuteanme/Booking_Ticket_Movie">
               https://github.com/longcuteanme/Booking_Ticket_Movie
             </a>
           </td>
